084a2a6e74e13e86286955aaed0711ad589ecb9d
Jokull Johannsson <jokull@kth.se>
2016-04-20 19:36:28 +0200

    No quite working, but almost there.

--- DataGenerator.py
+++ DataGenerator.py
@@ -36,19 +36,6 @@ def createDataFile(data):
 	data_size = ','.join(map(str,data.shape))
 	np.savetxt("data.csv", data,fmt='%10.8f', delimiter=",",header=data_size)
 
-def createDataFile2(data):
-	print(list(data.shape))
-	np.insert(data, 0, list(data.shape))
-	np.array(list(data.shape)).tofile("data.bin",sep="")
-	np.array(list(data.shape)).tofile(open('data2.bin', 'wb'))
-	'''
-	newfile = open("data.dat",'wb')
-	data_size = ','.join(map(str,data.shape))
-	newfile.write(bytes(data_size+'\n', 'UTF-8'))
-	for row in data:
-		newfile.write(bytes(','.join(str(v) for v in value_list)+'\n', 'UTF-8'))
-	newfile.close()
-	'''
 
 def createDataFile3(data):
 	import struct
@@ -57,7 +44,7 @@ def createDataFile3(data):
 	s = struct.pack('i'*len(sizeList),*sizeList)
 	out_file.write(s)
 	for row in data:
-		s = struct.pack('f'*len(row),*row)
+		s = struct.pack('d'*len(row),*row)
 		out_file.write(s)
 	out_file.close()
 
@@ -65,9 +52,10 @@ def createDataFile3(data):
 ##Create a function that creates many clusters and returns a vector with all the clusters
 #
 #data = np.random.uniform(0,1,size=(200,3))
-data = generateClusters(2,3,10)
+np.random.seed(60)
+data = generateClusters(2,3,30)
 #print(data[0])
 createDataFile3(data)
 createDataFile(data)
 print(data)
-#plotClusters(data)
+plotClusters(data)
--- kMeans_MPI.c
+++ kMeans_MPI.c
@@ -3,36 +3,37 @@
 #include <stdlib.h>
 #include <math.h>
 #include <string.h>
+#include <limits.h>
 
-
+MPI_Status status;
 double** local_datapoints;  // The datapoints to cluster
 unsigned char* local_membership;      // Which cluster each datapoint belongs to
 double** local_newCentroids; // Sum of the dimensions of datapoints belonging to each cluster
 int* local_newCentroidsSize;  // Number of datapoints belonging to each cluster
 double** global_centroids;    // Means of each cluster
-
+int* global_centroidsSize;    
 
 int global_datapoint_count, processCount,Ip,rank;
 int local_datapoint_count;
-int numDims = 0;
-int K = 2;
+int numDims;
+int K, tag;
 
-float** allocMatrix(int rows, int cols, int useCalloc)
+double** allocMatrix(int rows, int cols, int useCalloc)
 {
     int i;
-    int header= rows *sizeof(float *);
-    int data=rows*cols*sizeof(float);
-    float ** rowptr;
+    int header= rows *sizeof(double *);
+    int data=rows*cols*sizeof(double);
+    double ** rowptr;
     if(useCalloc == 1) {
-        rowptr=(float **)malloc(header+data);
+        rowptr = (double **) calloc(header+data, sizeof(double));
     }
     else {
-        rowptr=(float **)malloc(header+data);    
+        rowptr=(double **)malloc(header+data);    
     }
     if(rowptr==NULL){
         return NULL;
     }
-    float * buf=(float*)(rowptr+rows);
+    double * buf=(double*)(rowptr+rows);
     for(i=0;i<rows;i++){
         rowptr[i]=buf+i*cols;
     } 
@@ -77,42 +78,162 @@ void mpi_read(char* filename)
     divd = global_datapoint_count / processCount;
     rem  = global_datapoint_count % processCount;
     len  = (rank < rem) ? rank*(divd+1) : rank*divd + rem;
-    disp = 2 * sizeof(int) + len * numDims * sizeof(float);
+    disp = 2 * sizeof(int) + len * numDims * sizeof(double);
 
     //Calculate the local size
     local_datapoint_count = (rank < rem) ? divd+1 : divd;
 
     //allocate a matrix to store the datapoints
-    float **local_datapoints = allocMatrix(local_datapoint_count, numDims, 1);
+    local_datapoints = allocMatrix(local_datapoint_count, numDims, 1);
 
 
-    MPI_Type_contiguous(local_datapoint_count, MPI_FLOAT, &filetype);
+    MPI_Type_contiguous(local_datapoint_count, MPI_DOUBLE, &filetype);
     MPI_Type_commit(&filetype);
 
-    MPI_File_set_view(fh, disp, MPI_FLOAT, filetype, "native", MPI_INFO_NULL);
-    MPI_File_read_all(fh, local_datapoints[0], local_datapoint_count*numDims, MPI_FLOAT, &status);
+    MPI_File_set_view(fh, disp, MPI_DOUBLE, filetype, "native", MPI_INFO_NULL);
+    MPI_File_read_all(fh, local_datapoints[0], local_datapoint_count*numDims, MPI_DOUBLE, &status);
     MPI_Type_free(&filetype);
     MPI_File_close(&fh);
-
+    /*
     printf("numObjs %d\n", global_datapoint_count);
     printf("numCoords %d\n", numDims);
     for(int i = 0; i < local_datapoint_count; i++){
-        printf("Rank %d has [%f, %f, %f]\n",rank, local_datapoints[i][0], local_datapoints[i][1], local_datapoints[i][2]);
+        printf("Rank %d has [%.8lf, %.8lf, %.8lf]\n",rank, local_datapoints[i][0], local_datapoints[i][1], local_datapoints[i][2]);
     }
-
+    */
 }
+
+double calcDistance(int pointIndex, int centroidIndex) {
+    double sum = 0.0;
+    for(int i = 0; i < numDims; i++) {
+        sum = sum + pow(local_datapoints[pointIndex][i] - global_centroids[centroidIndex][i], 2);
+    }
+    return sqrt(sum);
+}
+
+void sumDatapointAndNewCentroid(pointIndex, centroidIndex) {
+    for(int i = 0; i < numDims; i++) {
+        local_newCentroids[centroidIndex][i] += local_datapoints[pointIndex][i];
+    }
+}
+
+void calcNewCentroids() {
+
+    MPI_Allreduce(local_newCentroids[0], global_centroids[0], K*numDims, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
+    MPI_Allreduce(local_newCentroidsSize, global_centroidsSize, K, MPI_INT, MPI_SUM, MPI_COMM_WORLD);
+    //printf("Rank %d has centroidsSize [%d, %d]\n",rank, global_centroidsSize[0], global_centroidsSize[1]);
+
+    for(int k = 0; k < K; k++) {
+        for(int d = 0; d < numDims; d++) {
+            global_centroids[k][d] = global_centroids[k][d] / global_centroidsSize[k];
+            local_newCentroids[k][d] = 0.0;
+        }
+        local_newCentroidsSize[k] = 0;
+    }
+}
+
+void kMeans() {
+    int numChanged = 0; // Tracks how many datapoints changed clusters each iteration
+    double minDistance;
+    double distance = 0;
+    unsigned char curCluster;
+
+    for(int i = 0; i < 10; i++) { // nubmer of iterations
+        numChanged = 0; // Tracks how many datapoints changed clusters each iteration
+        for(int pointIndex = 0; pointIndex < local_datapoint_count; pointIndex++) { // iterate through datapoints
+            minDistance = LONG_MAX;
+
+            for(int k = 0; k < K; k++) { // iterate throught cluster centroids
+                distance = calcDistance(pointIndex, k);
+
+                if(distance < minDistance) {
+
+                    minDistance = distance;
+                    curCluster = k;
+                }
+            }
+            
+            if(local_membership[pointIndex] != curCluster) {
+                numChanged += 1;
+                local_membership[pointIndex] = curCluster;
+            }
+            sumDatapointAndNewCentroid(pointIndex, curCluster);
+            local_newCentroidsSize[curCluster] = local_newCentroidsSize[curCluster] + 1;
+        }
+
+        // calculate new cluster centers
+        calcNewCentroids();
+        //printCentroids();
+        //printf("Datapoints that changed clusters: %d\n", numChanged);
+    }
+}
+
+void printMemberships(){
+    int nothing = 0;
+    if(rank == 0){
+        
+        for(int i = 0; i < local_datapoint_count; i++){
+            //printf("Rank %d -> %d\n",rank, local_membership[i]);
+            printf("%d",rank);
+        }
+        
+        //printf("(%d)\n",rank);
+        MPI_Send(&nothing, 1, MPI_INT, rank+1, tag, MPI_COMM_WORLD);
+    }else{
+        
+        MPI_Recv(&nothing, 1, MPI_INT, rank-1, tag, MPI_COMM_WORLD, &status);
+        //printf("(%d)\n",rank);
+        
+        for(int i = 0; i < local_datapoint_count; i++){
+            //printf("Rank %d -> %d\n",rank, local_membership[i]);
+            printf("%d",rank);
+        }
+        if(rank < processCount-1){
+            MPI_Send(&nothing, 1, MPI_INT, rank+1, tag, MPI_COMM_WORLD);
+        }
+        
+    }
+}
+
+void initVars() {
+    local_membership = (unsigned char *) calloc(local_datapoint_count, sizeof(unsigned char));
+    global_centroids    = allocMatrix(K, numDims, 1);
+    local_newCentroids = allocMatrix(K, numDims, 1);
+    local_newCentroidsSize = (int *) calloc(K, sizeof(int));
+    global_centroidsSize = (int *) calloc(K, sizeof(int));
+    if(rank == 0){
+        for(int k = 0; k < K; k++) {
+            for(int i = 0; i < numDims; i++) {
+                global_centroids[k][i] = local_datapoints[k][i];
+            }
+        }
+    }
+    MPI_Bcast(global_centroids[0], K*numDims, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+    /*
+    for(int i = 0; i < K; i++){
+        printf("Rank %d has centroids(%d) [%.8lf, %.8lf, %.8lf]\n",rank,i, global_centroids[i][0], global_centroids[i][1], global_centroids[i][2]);
+    }
+    */
+}
+
 
 
 int main(int argc, char *argv[])
 {
-    MPI_Status status;
     int rc;
     rc = MPI_Init(&argc, &argv);
     rc = MPI_Comm_size(MPI_COMM_WORLD, &processCount);
     rc = MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-
+    K = 2;
+    
 
     mpi_read("data.bin");
+    initVars();
+    kMeans();
+    printMemberships();
+
+
+
     MPI_Finalize();
     //printf("Total sum is %d \n", totalSum);
     //printf("process %d just finished \n", rank);
